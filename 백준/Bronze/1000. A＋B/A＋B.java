3173664095422f9cd0b6e3ee2ff96e690bb9a82c
BaekDoHyeon <qorehgus0823@naver.com>
2024-06-24 13:54:27 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int valueA = in.nextInt();
+        int valueB = in.nextInt();
+
+        System.out.println(valueA+valueB);
+    }
+}
