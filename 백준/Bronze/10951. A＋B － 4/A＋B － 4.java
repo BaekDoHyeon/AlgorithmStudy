133c0047b91a6375e4aa8a9f325bc90d363a47d1
BaekDoHyeon <qorehgus0823@naver.com>
2024-07-05 14:46:01 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(true) {
+            String input = br.readLine();
+            if (input == null || input.isEmpty()) break;
+
+            String[] tmp = input.split(" ");
+            int num1 = Integer.parseInt(tmp[0]);
+            int num2 = Integer.parseInt(tmp[1]);
+
+            bw.write(num1 + num2 + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
