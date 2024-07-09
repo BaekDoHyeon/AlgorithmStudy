90e94a7b3709680a86fc78981fca2119044c7731
BaekDoHyeon <qorehgus0823@naver.com>
2024-07-09 17:03:35 +0900

    [Bronze II] Title: 다이얼, Time: 200 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        char[] charArray = str.toCharArray();
+        int totalTime = 0;
+
+        for (char c : charArray) {
+            switch (c) {
+                case 'A': case 'B': case 'C':
+                    totalTime += 3;
+                    break;
+                case 'D': case 'E': case 'F':
+                    totalTime += 4;
+                    break;
+                case 'G': case 'H': case 'I':
+                    totalTime += 5;
+                    break;
+                case 'J': case 'K': case 'L':
+                    totalTime += 6;
+                    break;
+                case 'M': case 'N': case 'O':
+                    totalTime += 7;
+                    break;
+                case 'P': case 'Q': case 'R': case 'S':
+                    totalTime += 8;
+                    break;
+                case 'T': case 'U': case 'V':
+                    totalTime += 9;
+                    break;
+                case 'W': case 'X': case 'Y':  case 'Z':
+                    totalTime += 10;
+                    break;
+            }
+        }
+        System.out.println(totalTime);
+    }
+}
