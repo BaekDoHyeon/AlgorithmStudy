47b894875348e2648697a233fc8c9661439dd50d
BaekDoHyeon <qorehgus0823@naver.com>
2024-07-18 17:15:44 +0900

    Update 탑.java

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -8,20 +8,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // 탑의 정보를 담기 위해 Stack<int[]> 선언
+        // 0번째 인덱스에는 탑의 순서(번호), 1번째 인덱스에는 탑의 높이가 저장된다.
         Stack<int[]> stack = new Stack<>();
 
-        //탑의 개수를 입략
+        //탑의 개수를 입력
         int topCnt = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        
         for (int i = 1; i <= topCnt; i++) {
             int tower = Integer.parseInt(st.nextToken());
 
+            // 처음 들어온 탑이 아니라면 -> 최소 탑의 번호는 2~
             while (!stack.isEmpty()) {
+                // stack 맨위에 있는 탑의 높이가 현재 들어온 탑의 높이보다 크다면 (레이저 수신 가능)
                 if (stack.peek()[1] > tower) {
+                    //현재 들어온 탑의 번호에 레이저 수신이 가능한 stack에 있는 탑의 번호(인덱스)를 저장
                     sb.append(stack.peek()[0]).append(" ");
                     break;
                 }
+                //기존에 있던 마지막 탑을 제거
                 stack.pop();
             }
 
