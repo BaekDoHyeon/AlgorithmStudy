37da46dbb80b4dc69d14a5c1d2ac06c3f4b0aec8
BaekDoHyeon <qorehgus0823@naver.com>
2024-07-27 17:40:23 +0900

    [Silver II] Title: 쇠막대기, Time: 344 ms, Memory: 23316 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+// 문제가 쪼~금 길긴해서 README.md 파일 작성 및 참고
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //괄호를 입력받는다.
+        String str = sc.next();
+
+        System.out.println(solution(str));
+    }
+
+    static int solution(String str) {
+        int result = 0;
+        Stack<Character> stack = new Stack<>();
+        char[] arr = str.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(') stack.push(arr[i]);
+            else if (arr[i] == ')') {
+                if (arr[i-1] == '(') {
+                    stack.pop();
+                    result += stack.size();
+                } else if (arr[i-1] == ')') {
+                    stack.pop();
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
