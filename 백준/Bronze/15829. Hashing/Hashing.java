6e6abc2686481249cc7d59aaaaf16e2c310af26f
BaekDoHyeon <qorehgus0823@naver.com>
2024-12-15 14:06:14 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17748 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,19 +2,21 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    final static int R = 31;
+    final static long R = 31;
+    final static long M = 1234567891;
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        
+        StringBuilder sb = new StringBuilder();
+
         int L = sc.nextInt();
         String str = sc.next();
-        long sum = 0;
-
+        long result = 0;
+        long pow = 1;
         for (int i = 0; i < L; i++) {
-            char c = str.charAt(i);
-            sum += ((int) c - 96) * Math.pow(R, i);
+            result += ((str.charAt(i) - 96) * pow);
+            pow = (pow * 31) % M;
         }
 
-        System.out.println(sum);
+        System.out.println(result % M);
     }
 }
