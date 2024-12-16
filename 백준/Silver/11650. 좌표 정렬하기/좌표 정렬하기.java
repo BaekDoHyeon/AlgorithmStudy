bbbbcdd9dcaeabff91d7cca09103650cc928f915
BaekDoHyeon <qorehgus0823@naver.com>
2024-12-16 17:30:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1828 ms, Memory: 193696 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Pos implements Comparable<Pos> {
+    int x;
+    int y;
+
+    Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Pos o) {
+        if (this.x == o.x) return this.y - o.y;
+        else return this.x - o.x;
+    }
+}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = sc.nextInt();
+
+        List<Pos> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) list.add(new Pos(sc.nextInt(), sc.nextInt()));
+        Collections.sort(list);
+
+        for (Pos pos : list) {
+            System.out.println(pos.x + " " + pos.y);
+        }
+    }
+}
