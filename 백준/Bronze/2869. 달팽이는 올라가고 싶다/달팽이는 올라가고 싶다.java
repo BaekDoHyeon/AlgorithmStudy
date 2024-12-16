ec445a35c992995c54894d356d4556ec0ccfa9b1
BaekDoHyeon <qorehgus0823@naver.com>
2024-12-16 14:51:14 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 시간 초과...
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        === 시간 초과 코드 ===
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+//        int V = sc.nextInt();
+//        int sum = 0;
+//        int day = 0;
+//        while(true) {
+//            sum += A;
+//            day++;
+//            if (sum >= V) break;
+//            sum -= B;
+//        }
+//
+//        System.out.println(day);
+//        === 시간 초과 코드 ===
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) day++;
+
+        System.out.println(day);
+    }
+}
